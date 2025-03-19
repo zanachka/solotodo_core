18d6654c054fd5e16d0297cd3981003ac71c9809
Vijay Khemlani <vj@solotodo.com>
2025-03-19 14:38:24 -0300

    * Updated brands_unicode functions

--- solotodo/metamodel_custom_functions/brand_unicode.py
+++ solotodo/metamodel_custom_functions/brand_unicode.py
@@ -1,19 +1,18 @@
 def brand_unicode(elastic_search_original, model_name):
-    if 'brand_unicode' in elastic_search_original:
+    if "brand_unicode" in elastic_search_original:
         return {}
 
     # Try common combinations (line.brand, line.brand.family)
 
     patterns = [
-        'line_brand_unicode',
-        'line_family_brand_unicode',
-        'family_line_brand_unicode',
+        "line_brand_unicode",
+        "line_family_brand_unicode",
+        "family_line_brand_unicode",
+        "commercial_line_family_brand_unicode",
     ]
 
     for pattern in patterns:
         try:
-            return {
-                'brand_unicode': elastic_search_original[pattern]
-            }
+            return {"brand_unicode": elastic_search_original[pattern]}
         except KeyError:
             pass
