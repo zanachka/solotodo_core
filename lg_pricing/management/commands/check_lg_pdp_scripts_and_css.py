e35617640a94632a0106fb23c94d52f990759e0d
Vijay Khemlani <vj@solotodo.com>
2025-04-07 13:52:12 -0400

    * Added management command to check if our JS and CSS assets for the lg_emotional_pdps are up to date

---
+++ lg_pricing/management/commands/check_lg_pdp_scripts_and_css.py
@@ -0,0 +1,39 @@
+from django.core.mail import send_mail
+from django.core.management import BaseCommand
+
+from storescraper.stores import LgCl
+
+from storescraper.utils import session_with_proxy
+
+from solotodo.models import SoloTodoUser
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        try:
+            extra_args = LgCl.preflight()
+            target_url = "https://www.lg.com/cl/tvs-y-soundbars/4k-uhd-tvs/55ut8050psb/"
+            session = session_with_proxy(extra_args)
+            response = session.get(target_url)
+            checks = [
+                "198646195fd16dfedce75f3ff6bc8708",
+                "925ac26efc5402996eae22777ae34362",
+                "edd239717c86",
+            ]
+            for check in checks:
+                if check not in response.text:
+                    raise Exception(f"{check} not found in {target_url}")
+
+            print("Checks OK")
+        except Exception as e:
+            email_recipients = [
+                x.email_recipient_text()
+                for x in SoloTodoUser.objects.filter(is_superuser=True)
+            ]
+
+            send_mail(
+                "Error detecting LG PDP assets",
+                str(e),
+                SoloTodoUser().get_bot().email_recipient_text(),
+                email_recipients,
+            )
