4724e9ba0448a86ea5c72b4dd892060dfcc497ee
Vijay Khemlani <vj@solotodo.com>
2025-05-25 10:07:18 -0400

    * Added warning logging * Reset cache counter whe running store update pricing

--- solotodo/models/store.py
+++ solotodo/models/store.py
@@ -417,6 +417,9 @@ class Store(models.Model):
         logger.info(json.dumps(logging_payload))
 
         if use_async:
+            cache.set(f"{self.id}_discover_entries", 0, expire=60 * 60)
+            cache.set(f"{self.id}_products_for_url", 0, expire=60 * 60)
+
             update_log.initialize_task_counter(0)
             for category in categories:
                 update_log.increment_task_counter()
