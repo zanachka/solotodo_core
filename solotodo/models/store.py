0afb5d424b3b69ffec3f99b15561ad90a171efd4
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2025-06-05 17:13:23 -0400

    add preflight in section_positions

--- solotodo/models/store.py
+++ solotodo/models/store.py
@@ -616,6 +616,8 @@ class Store(models.Model):
         else:
             extra_args = self.storescraper_extra_args_as_json()
 
+        extra_args = self.scraper.extra_args_with_preflight(extra_args)
+
         if not sections:
             sections = self.scraper.sections()
 
